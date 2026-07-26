01e0ef7c45eccff22819b5c0505b4c8548ea2cec
AlinC10 <alincalcan@gmail.com>
2026-07-26 23:03:19 +0300

    Implement DOCX and TXT loaders and splitters
    - implement loaders for DOCX and TXT files using MarkItDown, python-docx
    and python's read file mode
    - improved splitter quality by building a document splitter class, using
    Markdown headers splitter and recursive character text splitter for
    DOCX and PDF files

--- doc_splitter.py
+++ doc_splitter.py
@@ -1,14 +1,66 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 
 
-def doc_splitter(chunk_size: int = 1000, chunk_overlap: int = 100,
-                 separators: list[str] = ["\n\n", "\n", "(?<=[.?!])", " ", ""]):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        separators=separators,
-        is_separator_regex=True,
-        keep_separator=True
-    )
+class DocSplitter:
+    def __init__(self, extension: str, chunk_size: int = 1000, chunk_overlap: int = 100,
+                               separators: list[str] | None = None):
+        if separators is None:
+            separators = ["\n\n", "\n", "(?<=[.?!])", " ", ""]
 
-    return splitter
+        self.recursive_doc_splitter = DocSplitter.recursive_doc_splitter(chunk_size, chunk_overlap, separators)
+        self.extension = extension
+        self.markdown_splitter = None
+
+        if self.extension in ['pdf', 'docx']:
+            self.markdown_splitter = DocSplitter.markdown_doc_splitter()
+
+    @staticmethod
+    def recursive_doc_splitter(chunk_size: int = 1000, chunk_overlap: int = 100,
+                               separators: list[str] | None = None):
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            separators=separators,
+            keep_separator=True
+        )
+
+        return splitter
+
+    @staticmethod
+    def markdown_doc_splitter():
+        headers_to_split_on = [
+            ("#", "Header 1"),
+            ("##", "Header 2"),
+            ("###", "Header 3"),
+        ]
+
+        splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=headers_to_split_on,
+            strip_headers=False,
+        )
+
+        return splitter
+
+    def document_split(self, document: Document) -> list[Document]:
+        """Split documents for the RAG system based on their type.
+        If the file send to the system is PDF or DOCX, it's first divided into Markdown sections, then after
+        sections numbers of characters using RecursiveCharacterTextSplitter.
+        If the file send is a TXT, it will be divided using only RecursiveCharacterTextSplitter.
+        """
+        content = document.page_content
+        metadata = document.metadata
+
+        docs = None
+
+        if self.markdown_splitter is not None:
+            docs = self.markdown_splitter.split_text(content)
+        else:
+            docs = [document]
+
+        for doc in docs:
+            doc.metadata.update(metadata)
+
+        recursive_split_docs = self.recursive_doc_splitter.split_documents(docs)
+
+        return recursive_split_docs
--- document_processor.py
+++ document_processor.py
@@ -1,121 +1,196 @@
 import os.path
 
-from langchain_community.document_loaders.parsers import RapidOCRBlobParser
+import docx
+# from langchain_community.document_loaders.parsers import RapidOCRBlobParser
 from langchain_core.documents import Document
 from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+from markitdown import MarkItDown
 from pymupdf4llm.ocr import rapidocr_api
 
-from doc_splitter import doc_splitter
+from doc_splitter import DocSplitter
 
-default_important_metadata_categories: list[str] = ['title', 'author', 'subject', 'keywords', 'page']
+default_important_metadata_categories: list[str] = ['title', 'author', 'subject', 'keywords', 'source',
+                                                    'file_path']
+page_break_delimiter = "\n---PAGE BREAK---\n"
 
 
-class DocumentProcessor:
-    def __init__(self):
-        pass
+def split_doc(docs: Document, extension: str, chunk_size: int = 1000, chunk_overlap: int = 100,
+              separator: list[str] | None = None) -> list[Document]:
+    """Split the documents based on the rules defined in the DocSplitter object."""
 
-    @staticmethod
-    def split_doc(docs: list[Document], chunk_size: int = 1000, chunk_overlap: int = 100) -> list[Document]:
-        splitter = doc_splitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        splitted_docs = splitter.split_documents(docs)
+    splitter = DocSplitter(extension, chunk_size, chunk_overlap, separator)
 
-        return splitted_docs
+    split_docs = splitter.document_split(docs)
 
-    @staticmethod
-    def check_file_extension(file_path: str):
-        _, extension = os.path.splitext(file_path)
-        extension = extension[1:]
+    return split_docs
 
-        accepted_types = ["doc", "docx", "pdf", "txt"]
 
-        if extension in accepted_types:
-            return extension
+def check_file_extension(file_path: str):
+    _, extension = os.path.splitext(file_path)
+    extension = extension[1:]
 
-        raise TypeError(f"Accepted document types: {", ".join(accepted_types)}")
+    accepted_types = ["docx", "pdf", "txt"]
 
-    @staticmethod
-    def _remove_useless_info_from_metadata(docs: list[Document],
-                                           important_metadata_categories: list[str] = default_important_metadata_categories) -> \
-            list[Document]:
-        """Remove useless information from metadata"""
-        for doc in docs:
-            keys_to_delete = [key for key in doc.metadata if key not in important_metadata_categories]
+    if extension in accepted_types:
+        return extension
 
-            for key in keys_to_delete:
-                del doc.metadata[key]
+    raise TypeError(f"Accepted document types: {", ".join(accepted_types)}")
 
-        return docs
 
-    @staticmethod
-    def _add_document_metadata(docs: list[Document], file_path: str, chat_id: str):
-        file_name = os.path.basename(file_path)
-        for doc in docs:
-            doc.metadata['source'] = file_name
-            doc.metadata['conversation_id'] = chat_id
+def _remove_useless_info_from_metadata(docs: Document,
+                                       important_metadata_categories: list[str] | None = None) -> Document:
+    """Remove useless information from metadata"""
 
-        return docs
+    if important_metadata_categories is None:
+        important_metadata_categories = default_important_metadata_categories
 
-    @staticmethod
-    def load_doc(file_path: str, chat_id: str, file_name: str | None = None,
-                 important_metadata_categories: list[str] = default_important_metadata_categories) -> list[Document]:
+    keys_to_delete = [key for key in docs.metadata if key not in important_metadata_categories]
 
-        """Checks file extension"""
-        extension = DocumentProcessor.check_file_extension(file_path)
+    for key in keys_to_delete:
+        del docs.metadata[key]
 
-        docs = None
-        if extension in ['doc', 'docx']:
-            docs = DocumentProcessor._load_docx(file_path)
-        elif extension == "pdf":
-            docs = DocumentProcessor._load_pdf(file_path)
-        else:
-            docs = DocumentProcessor._load_txt(file_path)
+    return docs
 
-        splitted_docs = DocumentProcessor.split_doc(docs)
 
-        splitted_docs = DocumentProcessor._remove_useless_info_from_metadata(splitted_docs,
-                                                                             important_metadata_categories)
+def _add_document_metadata(docs: list[Document], chat_id: str, extension: str):
+    """Add chat id attribute to the document metadata for better filtering in the database collection.
+    If the document added it's a PDF, it will also add the page number into the metadata.
+    """
 
-        return DocumentProcessor._add_document_metadata(splitted_docs, file_path, chat_id)
+    page_number = None
 
-    @staticmethod
-    def _load_docx(file_path: str):
-        pass
+    if extension == "pdf":
+        page_number = 1
 
-    @staticmethod
-    def _load_txt(file_path: str):
-        pass
+    for doc in docs:
+        doc.metadata['chat_id'] = chat_id
 
-    @staticmethod
-    def _load_pdf(file_path: str) -> list[Document]:
-        """Load the PDF file into the RAM using PyMuPDF4LLMLoader with
-        RapidOCR for retrieving text from images."""
-        loader = PyMuPDF4LLMLoader(
-            file_path=file_path,
-            mode="page",
-            extract_images=True,
+        if page_number:
+            if page_break_delimiter in doc.page_content:
+                page_number += 1
 
-            # use RapidOCR for extracted image fragments
-            images_parser=RapidOCRBlobParser(),
+            doc.metadata['page'] = page_number
 
-            # tell PyMuPDF4LLM to use RapidOCR instead of Tesseract for scanned pages
-            ocr_function=rapidocr_api.exec_ocr
-        )
+    return docs
 
-        return loader.load()
 
-    @staticmethod
-    def format_for_llm(docs: list[Document]) -> str:
-        formated_context = "\n\n".join(
-            f"""[Source: {doc.metadata.get('source', 'Unknown')} | Page: {doc.metadata.get('page', 'N/A')}]\n{doc.page_content}"""
-            for doc in docs
-        )
+def load_doc(file_path: str, chat_id: str, file_name: str | None = None,
+             important_metadata_categories: list[str] | None = None) -> list[Document]:
+    if important_metadata_categories is None:
+        important_metadata_categories = default_important_metadata_categories
 
-        return formated_context
+    # Checks file extension
+    extension = check_file_extension(file_path)
 
-    # @staticmethod
-    # def process_docs(file_path:str, chunk_size: int = 1000, chunk_overlap: int = 100, important_metadata_categories: list[str]=default_important_metadata_categories) -> list[Document]:
-    #     docs = DocumentProcessor.load_doc(file_path ,important_metadata_categories)
-    #
-    #     splitted_docs = DocumentProcessor.split_doc(docs, chunk_size, chunk_overlap)
-    #
-    #     return splitted_docs
+    docs = None
+    if extension in 'docx':
+        docs = _load_docx(file_path, chat_id)
+    elif extension == "pdf":
+        docs = _load_pdf(file_path, chat_id)
+        docs = _remove_useless_info_from_metadata(docs, important_metadata_categories)
+    else:
+        docs = _load_txt(file_path, chat_id)
+
+    split_docs = split_doc(docs, extension)
+
+    return _add_document_metadata(split_docs, file_path, chat_id)
+
+
+def extract_file_name(file_path: str) -> str:
+    """Extract the fie name from a given file path.
+    e.g.: pdf/file_name.pdf = file_name
+    """
+
+    base_name = os.path.basename(file_path)
+    name_without_ext = os.path.splitext(base_name)[0]
+
+    return name_without_ext
+
+
+def extract_file_title(file_path: str, text: str = "", meta_title: str | None = None):
+    """Finds the best possible title using a fallback chain."""
+
+    if meta_title and meta_title.strip() not in ["", "Untitled"]:
+        return meta_title
+
+    # search for "# " which is used for titles in Markdown format
+    for line in text.splitlines():
+        line = line.strip()
+        if line.startswith('# '):
+            return line[2:].strip(" *")
+
+    # create the title from file path, removing "_" and "-" symbols
+    # e.g.: pdf/file_name.pdf = File Name
+    file_name_without_ext = extract_file_name(file_path)
+    clean_name = file_name_without_ext.replace("_", " ").replace("-", " ").title()
+
+    return clean_name
+
+
+def _load_docx(file_path: str, chat_id: str) -> Document:
+    """Load the .docx file using MarkItDown library."""
+
+    md = MarkItDown()
+    document = md.convert(file_path).text_content
+
+    # load .docx properties
+    doc = docx.Document(file_path)
+    props = doc.core_properties
+
+    metadata = {"author": props.author or "Unknown",
+                "title": extract_file_title(file_path, document, props.title),
+                "category": props.category or "Unknown",
+                "source": file_path,
+                "chat_id": chat_id
+                }
+
+    return Document(
+        page_content=document,
+        metadata=metadata
+    )
+
+
+def _load_txt(file_path: str, chat_id: str) -> Document:
+    """Load the .txt file.
+    If Python encounters a corrupted or weird character that isn't valid UTF-8, instead of crashing the entire
+    script with a UnicodeDecodeError, it simply replaces the bad character with a standard placeholder symbol (
+    usually ``).
+    """
+    with open(file_path, mode="r", encoding="utf-8", errors="replace") as file:
+        txt = file.read()
+        return Document(page_content=txt,
+                        metadata={"source": file_path, "title": extract_file_title(file_path), "chat_id": chat_id})
+
+
+def _load_pdf(file_path: str, chat_id: str) -> Document:
+    """Load the PDF file into the RAM using PyMuPDF4LLMLoader with
+    RapidOCR for retrieving text from images."""
+    loader = PyMuPDF4LLMLoader(
+        file_path=file_path,
+        mode="page",
+        pages_delimiter=page_break_delimiter,
+        # extract_images=True,
+        extract_images=False,
+
+        # use RapidOCR for extracted image fragments
+        # images_parser=RapidOCRBlobParser(),
+
+        # tell PyMuPDF4LLM to use RapidOCR instead of Tesseract for scanned pages
+        ocr_function=rapidocr_api.exec_ocr
+    )
+
+    document = loader.load()[0]
+    document.metadata.update({"chat_id": chat_id})
+
+    return document
+
+
+def format_for_llm(docs: list[Document]) -> str:
+    formated_context = "\n\n".join(
+        f"""[Source: {doc.metadata.get('title', 'Unknown')} 
+{f"| Page: {doc.metadata['page']}" if doc.metadata.get("page", False) else ""}]\n
+{doc.page_content}"""
+        for doc in docs
+    )
+
+    return formated_context
